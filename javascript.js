a0650b133cfe31fa1c2386b0302b4a22b241e897
Phillip Sim <phillipsim@phillips-air.mynetworksettings.com>
2023-10-13 15:59:00 -0400

    modified all html pages to incorporate nav bar changes, included media queries, dropdown, and z-index

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 let greet = "Hello, Welcome to my Personal Site!";
 console.log(greet);
-alert(greet);
+// alert(greet);
+
+function myFunction() {
+  console.log("hello");
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
